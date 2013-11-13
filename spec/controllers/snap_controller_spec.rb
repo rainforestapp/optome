4ddc58bbbe666e5068eacf2346b85c22090816d0
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2013-11-12 20:50:31 -0800

    add pending specs

--- spec/controllers/snap_controller_spec.rb
+++ spec/controllers/snap_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
-describe SdfgfdsfController do
-
+describe SnapController do
+  describe 'upload' do
+    it "should return 200 if successful"
+    it "should enqueue a job"
+    it "should return 400 if no photo provided"
+  end
 end
